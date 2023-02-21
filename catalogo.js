9789653690a3d53c0510a124ded5384f3c7d1b49
Jose Rojas <rojaslema25@gmail.com>
2023-02-21 18:40:10 -0300

    segundo commit

---
+++ catalogo.js
@@ -0,0 +1,132 @@
+
+
+const productos = [
+    {
+        nombre: 'Grand the auto V',
+        destino: 'PlayStation 5/PC/XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 10000
+    },
+    {
+        nombre: 'BlackDesert',
+        destino: 'PC',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 30
+    },
+    {
+        nombre: 'Albion online',
+        destino: 'PC',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 0
+    },
+    {
+        nombre: 'SpiderMan',
+        destino: 'PC/PlayStation5/XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 5000
+    },
+    {
+        nombre: 'Rust',
+        destino: 'PC',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 0
+    },
+    {
+        nombre: 'Los Sims',
+        destino: 'PC',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 3000
+    },
+    {
+        nombre: 'Sim City 3000',
+        destino: 'PC',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 900
+    },
+    {
+        nombre: 'Gears of war 5',
+        destino: 'XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 5000
+    },
+    {
+        nombre: 'Halo',
+        destino: 'XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 500
+    },
+    {
+        nombre: 'God of war',
+        destino: 'PlayStation5/XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 2000
+    },
+    {
+        nombre: 'Mortal Kombat',
+        destino: 'PlayStation5',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 200
+    },
+    {
+        nombre: 'Demon soul',
+        destino: 'PlayStation5',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 2000
+    },
+    {
+        nombre: 'Sonic',
+        destino: 'PlayStation5',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 500
+    },
+    {
+        nombre: 'Cod:Warzone2.1',
+        destino: 'PC/PlayStation5/XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 0
+    },
+    {
+        nombre: 'Fornite',
+        destino: 'PC/PlayStation5/XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 0
+    },
+    {
+        nombre: 'Cyber Punk',
+        destino: 'PC/PlayStation5/XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 6000
+    },
+    {
+        nombre: 'Need For Speed',
+        destino: 'PC/PlayStation5/XboxOner',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 500
+    },
+    {
+        nombre: 'Nioh',
+        destino: 'PC/PlayStation5/XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 900
+    },
+    {
+        nombre: 'Assassins Creed',
+        destino: 'PC/PlayStation5/XboxOne',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 100
+    },
+    {
+        nombre: 'Elden Ring',
+        destino: 'PC/PlayStation5',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 1000
+    },
+    {
+        nombre: 'Jedi Survivor',
+        destino: 'PlayStation5',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 2500
+    },
+]
+
+
